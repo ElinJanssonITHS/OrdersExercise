98bf966ef862d3f92a0d5983cbbbeb944b0bc182
Elin Jansson <99elinjansson@gmail.com>
2023-09-27 06:58:20 +0200

    Add project files.

---
+++ Orders.Common/Classes/OrderFactory.cs
@@ -0,0 +1,17 @@
+﻿using Orders.Common.Records;
+namespace Orders.Common.Classes;
+
+public class OrderFactory
+{
+    public List<Order> Orders { get; } = new ();
+    public void Add(string customerName, string address)
+    {
+        try
+        {
+            Orders.Add(new Order(Orders.Count+1, customerName, address));
+        }
+        catch { throw; }
+    }
+    public List<Order> Get() => Orders;
+    public Order? Latest () => Orders.LastOrDefault();
+}
---
+++ Orders.Common/Records/LineItem.cs
@@ -0,0 +1,58 @@
+﻿
+
+namespace Orders.Common.Records;
+
+public record LineItem
+{
+    private double _vat = default;
+    public int Id { get; } = default;
+    public int OrderId { get; } = default;
+    public string Product { get; } = string.Empty;
+    public int Quantity { get; } = default;
+    public double Price { get; } = default;
+    public double Vat => this switch
+    {
+        var (vat) when vat >= 1 && vat <= 100 => vat / 100,
+        var (vat) when vat > 0 && vat < 1 => vat,
+        _ => 0
+    };
+    public double Total => Quantity * Price * (Vat + 1);
+    public double VatCost => Quantity * Price * Vat;
+    public LineItem() { }
+    public LineItem(int orderId, int id, string product, int quantity, double price, double vat)
+    {
+        try
+        {
+            if (orderId < 1) throw new ArgumentException("The order ID must be greater than 0.");
+            else if (id < 1) throw new ArgumentException("The Id must be grater than 0.");
+            else if (product.Equals(string.Empty) || product == default) throw new ArgumentException("The product name cannot be empty.");
+            else if (quantity < 1) throw new ArgumentException("The quantity must be grater than 0");
+            else if (price <= 0) throw new ArgumentException("The price must be grater than or equal to 0.");
+            else if (vat < 0) throw new ArgumentException("The VAT must be grater than 0");
+
+            OrderId = orderId;
+            Id = id;
+            Product = product;
+            Quantity = quantity;
+            Price = price;
+            _vat = vat;
+        }
+        catch (Exception ex)
+        {
+            ex.Data.Add("Order Id", orderId);
+            ex.Data.Add("Line Item Id", id);
+            ex.Data.Add("Product", product);
+            ex.Data.Add("Quantity", quantity);
+            ex.Data.Add("Price", price);
+            ex.Data.Add("VAT", vat);
+            throw ex;
+
+        }
+    }
+    private void Deconstruct(out double vat) => (vat) = (_vat);
+
+
+
+
+
+}
